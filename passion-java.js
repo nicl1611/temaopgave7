bfd0416ddb79981a566de083a63d7b50c2476f96
Bitten94 <sarabitten.merlin@gmail.com>
2020-09-14 10:25:16 +0200

    Fjerner ubrugt js/kommentare

--- passion-java.js
+++ passion-java.js
@@ -43,21 +43,6 @@
                     klon.querySelector(".info").textContent = person.gsx$info.$t;
                     klon.querySelector(".boomerang").textContent = person.gsx$boomerang.$t;
                     klon.querySelector(".pris").textContent = person.gsx$pris.$t;
-                    //* klon.querySelector(".artistinstagram").textContent = person.gsx$artistinstagram.$t;
-                    //* klon.querySelector(".artistyoutube").textContent = person.gsx$artistyoutube.$t;
-                    //*  klon.querySelector(".artistgiphy").textContent = person.gsx$artistgiphy.$t;
-                    //* klon.querySelector(".artistwebsite").textContent = person.gsx$artistwebsite.$t;
-                    //* klon.querySelector(".artistshop").textContent = person.gsx$artistshop.$t;
-
-                    /*---   Filtre
-
-                    klon.querySelector(".color1").textContent = person.gsx$color1.$t;
-                    klon.querySelector(".color2").textContent = person.gsx$color2.$t;
-                    klon.querySelector(".color3").textContent = person.gsx$color3.$t;
-                    klon.querySelector(".motive1").textContent = person.gsx$motive1.$t;
-                    klon.querySelector(".motive2").textContent = person.gsx$motive2.$t;
-
-                        ---*/
 
                     klon.querySelector("img").src = "img/kvadratisk/" + person.gsx$image.$t + ".jpg";
 
