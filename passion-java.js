bf8f45124ed55d1d3d2f23539d83651e4a706674
nicl1611 <nicl1611@stud.kea.dk>
2020-09-14 16:18:32 +0200

    Burger done

--- passion-java.js
+++ passion-java.js
@@ -1,3 +1,30 @@
+window.addEventListener("load", sidenVises);
+
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#menu").classList.toggle("hide");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hide");
+
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "✖";
+    }
+
+}
+
+
 let filter = "alle";
 
 
