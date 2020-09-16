a39dec211f74853eec9b4031e8f4c03064d1a150
vict2586 <vict2586@stud.kea.dk>
2020-09-16 09:51:14 +0200

    Kommentarer i JavaScript

--- passion-java.js
+++ passion-java.js
@@ -1,104 +1,70 @@
 let filter = "alle";
 
 
-
 let personer;
 
 
-
 let container = document.querySelector("section");
+
 let temp = document.querySelector("template");
 
+
+//-------------------------------------------------------- Loading the html -------------------------------------------------------------//
 
 
 document.addEventListener("DOMContentLoaded", loadJSON);
 
 
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//---------------------------------------------------- sets link to spreadsheets -------------------------------------------------------//
 
 
 const link = "https://spreadsheets.google.com/feeds/list/1GvVRM4YQgG5vMIA_p453OByxwNFP4daER7aHthI9nhE/od6/public/values?alt=json";
 
 
-klon.querySelector(".id").textContent = person.gsx$id.$t;
-klon.querySelector(".info").textContent = person.gsx$info.$t;
-
-klon.querySelector(".boomerang").textContent = person.gsx$boomerang.$t;
-//* klon.querySelector(".artistinstagram").textContent = person.gsx$artistinstagram.$t;
-//* klon.querySelector(".artistyoutube").textContent = person.gsx$artistyoutube.$t;
-//*  klon.querySelector(".artistgiphy").textContent = person.gsx$artistgiphy.$t;
-//* klon.querySelector(".artistwebsite").textContent = person.gsx$artistwebsite.$t;
-//* klon.querySelector(".artistshop").textContent = person.gsx$artistshop.$t;
-
-//* klon.querySelector(".boomerang").textContent = person.gsx$boomerang.$t;
-klon.querySelector(".pris").textContent = person.gsx$pris.$t;
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//----------------------------------------------- loading json + lisents to button click -----------------------------------------------//
 
 
 async function loadJSON() {
+
     const JSONData = await fetch(link);
+
     personer = await JSONData.json();
+
     addEventListenersToButons();
+
     vis(personer);
+
     document.querySelector("#luk").addEventListener("click", () => popup.style.display = "none");
 }
 
 
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//------------------------------------------ tells template what to show + telling to show it -----------------------------------------//
 
 
 function vis(personer) {
 
-
-
     const templatePointer = document.querySelector("template");
-
-
 
     const listPointer = document.querySelector("section");
 
-
-
     listPointer.innerHTML = "";
-
-
 
     personer.feed.entry.forEach(person => {
 
-
-
         if (filter == "alle" || filter == person.gsx$kategori.$t) {
-
-
 
             console.log(person);
 
-
-
             const klon = templatePointer.cloneNode(true).content;
-
-
 
             klon.querySelector(".id").textContent = person.gsx$id.$t;
             klon.querySelector(".info").textContent = person.gsx$info.$t;
             //* klon.querySelector(".boomerang").textContent = person.gsx$boomerang.$t;
             klon.querySelector(".pris").textContent = person.gsx$pris.$t;
 
-
-
             klon.querySelector("img").src = "img/kvadratisk/" + person.gsx$image.$t + ".jpg";
 
-
-
             klon.querySelector("article").addEventListener("click", () => visDetaljer(person));
-
-
 
             listPointer.appendChild(klon);
         }
@@ -106,9 +72,7 @@ function vis(personer) {
 }
 
 
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//-------------------------------------------- tells what to show in popup + show it ----------------------------------------------------//
 
 
 function visDetaljer(person) {
@@ -129,9 +93,7 @@ function visDetaljer(person) {
 }
 
 
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//--------------------------------------------------------- lisents to click -----------------------------------------------------------//
 
 
 function addEventListenersToButons() {
@@ -141,32 +103,20 @@ function addEventListenersToButons() {
 }
 
 
-
-//-------------------------------------------------------------------------------------------------------------------------------------//
-
+//----------- filter after with botton was clicked + show with text what botton was clicked + show that botton was clicked ------------//
 
 
 function filterBTNs() {
     filter = this.dataset.kategori;
 
-
-
     document.querySelector("h1").textContent = this.textContent;
 
-
-
     document.querySelectorAll(".filter").forEach((btn) => {
-
-
 
         btn.classList.remove("valgt");
     })
 
-
-
     this.classList.add("valgt");
-
-
 
     vis(personer);
 }
